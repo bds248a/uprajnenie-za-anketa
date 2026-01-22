c88ec6b4b7c6f74bdcc6271be77664158ab21839
bds248a <bds24.8a@gmail.com>
2026-01-22 09:46:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 st.title("Анкета")
 
 text = st.text_input("Въведи текст")
-name = name.st.text_input("Въведи име")
-subject = subject.st.selectbox("Избери предмет", ["Математика", "Английски", "Български"])
-grade = grade.st.selectbox("Избери оценка"["6", "5", "4", "3", "2"])
-age = age.st.number_input("Въведи на колко години си")
+name = st.text_input("Въведи име")
+subject = st.selectbox("Избери предмет", ["Математика", "Английски", "Български"])
+grade = st.selectbox("Избери оценка"["6", "5", "4", "3", "2"])
+age = st.number_input("Въведи на колко години си")
 
-if "age" > 18:
-{
+if age > 18:
   st.write("Ти не си ученик")
-}
 else:
-{
   st.write("grade")
-}
+
