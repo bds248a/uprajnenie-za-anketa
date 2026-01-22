2a2d0302347f876568bff88d0dd1a6975fabad4b
bds248a <bds24.8a@gmail.com>
2026-01-22 09:48:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
+import streamlit as st
+
 st.title("Анкета")
 
 text = st.text_input("Въведи текст")
 name = st.text_input("Въведи име")
-subject = st.selectbox("Избери предмет", ["Математика", "Английски", "Български"])
-grade = st.selectbox("Избери оценка"["6", "5", "4", "3", "2"])
-age = st.number_input("Въведи на колко години си")
+
+subject = st.selectbox(
+    "Избери предмет",
+    ["Математика", "Английски", "Български"]
+)
+
+grade = st.selectbox(
+    "Избери оценка",
+    ["6", "5", "4", "3", "2"]
+)
+
+age = st.number_input("Въведи на колко години си", min_value=0)
 
 if age > 18:
-  st.write("Ти не си ученик")
+    st.write("Ти не си ученик")
 else:
-  st.write("grade")
-
+    st.write(f"Твоята оценка е: {grade}")
