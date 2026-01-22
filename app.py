950e4b1c1bc019ca6ea5754f4ea5aa80ac6904f7
bds248a <bds24.8a@gmail.com>
2026-01-22 09:43:39 +0200

    Enhance app.py with additional input fields
    Added inputs for name, grade, and age with validation.

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 st.title("Анкета")
 
 text = st.text_input("Въведи текст")
-subject.st.selectbox("Избери предмет", ["Математика", "Английски", "Български"])
+name = name.st.text_input("Въведи име")
+subject = subject.st.selectbox("Избери предмет", ["Математика", "Английски", "Български"])
+grade = grade.st.selectbox("Избери оценка"["6", "5", "4", "3", "2"])
+age = age.st.number_input("Въведи на колко години си")
+
+if "age" > 18
+{
+  st.write("Ти не си ученик")
+}
+else 
+{
+  st.write("grade")
+}
